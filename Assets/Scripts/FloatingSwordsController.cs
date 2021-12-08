3abc6af12293f4f33f63b7b795146768a5bc7b78
Zeyltar <florian.poinsot@ynov.com>
2021-12-09 00:45:35 +0100

    update FloatingSwordsController

--- Assets/Scripts/FloatingSwordsController.cs
+++ Assets/Scripts/FloatingSwordsController.cs
@@ -12,39 +12,69 @@ public class FloatingSwordsController : MonoBehaviour
     private GameObject SwordPrefab;
     [SerializeField]
     private int SwordsQuantity;
-    [SerializeField, Range(0, 360)]
+    [SerializeField, Range(0f, 360f)]
     private float Circumference;
-    [SerializeField, Range(0, 360)]
+    [SerializeField, Range(0f, 360f)]
     private float StartingAngle;
-    
-    
+    [SerializeField]
+    private float Scale;
+    [SerializeField]
+    private float RotationSpeed;
+
+    private bool isRotating;
+    private Vector3 idlePosition; 
+    static private List<GameObject> list;
 
     // Start is called before the first frame update
     void Start()
     {
-        float currentAngle = StartingAngle;
-        float radians;
-        Vector3 position;
-        Vector3 rotation;
-        GameObject go;
-        for (int i = 0; i < SwordsQuantity; i++)
-        {
-            currentAngle += Circumference / SwordsQuantity;
-            radians = currentAngle * Mathf.Deg2Rad;
-
-            position = new Vector3(Mathf.Cos(radians), 0, Mathf.Sin(radians)) * Radius;
-            go = Instantiate(SwordPrefab);
-            go.transform.SetParent(Container);
-            go.transform.position = Container.position + position;
-            go.transform.localScale = Vector3.one * 3;
-
-            //go.transform.rotation = Quaternion.Euler()
-        }    
+        isRotating = false;
+        idlePosition = transform.position;
+        list = new List<GameObject>();
+        StartCoroutine(SpawnSwords());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnSwords()
+    {
+        float currentAngle = StartingAngle;
+        float radians;
+        Vector3 position;
+        GameObject go;
+
+        for (int i = 0; i < SwordsQuantity; i++)
+        {
+            currentAngle += Circumference / SwordsQuantity;
+            radians = currentAngle * Mathf.Deg2Rad;
+            position = new Vector3(Mathf.Sin(radians), 0, Mathf.Cos(radians)) * Radius;
+
+            go = Instantiate(SwordPrefab);
+            go.name = go.name + " " + i;
+            go.transform.SetParent(Container);
+            go.transform.position = Container.position + position;
+            go.transform.localScale = Vector3.one * Scale;
+            go.transform.rotation = Quaternion.Euler(0, radians * Mathf.Rad2Deg - 270, 30);
+
+            list.Add(go);
+
+            yield return null;
+        }
+
+        isRotating = true;
+        StartCoroutine(RotateIdle());
+    }
+
+    IEnumerator RotateIdle()
+    {
+        while (isRotating)
+        {
+            Container.transform.Rotate(0, RotationSpeed * Time.deltaTime, 0);
+            yield return null;
+        }
     }
 }
